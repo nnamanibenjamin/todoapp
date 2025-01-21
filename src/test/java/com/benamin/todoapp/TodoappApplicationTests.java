4e17655c598ea8a07da484a7632d6a09f0c901ff
nnamanibenjamin <nnamanibenjamin180@gmail.com>
2025-01-21 11:38:11 +0100

    first commit

---
+++ src/test/java/com/benamin/todoapp/TodoappApplicationTests.java
@@ -0,0 +1,13 @@
+package com.benamin.todoapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TodoappApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
